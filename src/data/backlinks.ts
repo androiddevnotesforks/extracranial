9040ae2fede65b7d52a512d3eb8d388932ba73c1
Sunghyun Cho <hey@cho.sh>
2024-02-28 23:41:28 -0800

    wrote something

--- src/data/backlinks.ts
+++ src/data/backlinks.ts
@@ -1924,7 +1924,7 @@ export const backlinks = {
     },
     "Open Core Models": {
         "2024-02-28": "- [[Open Core Models]]",
-        "Newtonian Correspondence": "| 02-28 | `EN` [[Open Core Models]]      ..."
+        "Newtonian Correspondence": "| 02-28 | `EN` [[Open Core Models]] |     ..."
     },
     "Tianyi Chen et al. VAFL a Method of Vertical Asynchronous Federated Learning": {
         "Project Florence Literature Review": "- [ ] [[Tianyi Chen et al. VAFL a Method of Vertical Asynchronous Federated Learning]]"
